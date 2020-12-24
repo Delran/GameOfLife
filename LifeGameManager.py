883017f2cb6e56b3c40ee4fb60b3e43bce3f8d59
Delran <Delran@hotmail.fr>
2020-12-24 13:27:21 +0100

    Added lambda for linking the grid

--- LifeGameManager.py
+++ LifeGameManager.py
@@ -8,8 +8,9 @@ class LifeGameManager:
     __grid = []
     __length = 0
     __height = 0
+    __cyclePeriod = 0
 
-    def __init__( self, length, height ):
+    def __init__( self, length, height, period = 0.3 ):
 
         self.createGrid( length, height )
 
@@ -20,26 +21,20 @@ class LifeGameManager:
         self.__grid[1][3].setAlive()
         self.__grid[0][2].setAlive()
 
-    #Apply lambda on each node of the grid
-    def forEachNode( self, func ):
+    #Pass i and j as params to given lambda
+    #for each node in the grid
+    def __forEachNode( self, func ):
         for i in range( self.__height ):
             for j in range( self.__length ):
-                func(self.__grid[i][j])
+                func( i, j )
 
+    #Cycling, wait for a bit, compute and update every cells
+    #in the game's grid
     def cycle( self ):
         time.sleep(0.3)
 
-        self.forEachNode( lambda node : node.compute() )
-        self.forEachNode( lambda node : node.update() )
-
-        #for i in range( self.__height ):
-        #    for j in range( self.__length ):
-        #        self.__grid[i][j].compute()
-
-
-        #for i in range( self.__height ):
-        #    for j in range( self.__length ):
-        #        self.__grid[i][j].update()
+        self.__forEachNode( lambda i, j : self.__grid[i][j].compute() )
+        self.__forEachNode( lambda i, j : self.__grid[i][j].update() )
         self.printGrid()
 
     def createGrid( self, length, height ):
@@ -58,31 +53,32 @@ class LifeGameManager:
                 node = LifeNode()
                 row.append( node )
 
-        for i in range( self.__height ):
-            for j in range( self.__length ):
-                node = self.__grid[i][j]
-                #print("Acceccing node[{}][{}]".format(i-1%self.__height, j  %self.__length))
-                #print("Acceccing node[{}][{}]".format(i-1%self.__height, j+1%self.__length))
-                #print("Acceccing node[{}][{}]".format(i  %self.__height, j+1%self.__length))
-                #print("Acceccing node[{}][{}]".format(i+1%self.__height, j+1%self.__length))
-                #print("Acceccing node[{}][{}]".format(i+1%self.__height, j  %self.__length))
-                #print("Acceccing node[{}][{}]".format(i+1%self.__height, j-1%self.__length))
-                #print("Acceccing node[{}][{}]".format(i  %self.__height, j-1%self.__length))
-                #print("Acceccing node[{}][{}]".format(i-1%self.__height, j-1%self.__length))
-                linkArray = [
-                                self.__grid[(i-1)%self.__height][(j  )%self.__length],
-                                self.__grid[(i-1)%self.__height][(j+1)%self.__length],
-                                self.__grid[(i  )%self.__height][(j+1)%self.__length],
-                                self.__grid[(i+1)%self.__height][(j+1)%self.__length],
-                                self.__grid[(i+1)%self.__height][(j  )%self.__length],
-                                self.__grid[(i+1)%self.__height][(j-1)%self.__length],
-                                self.__grid[(i  )%self.__height][(j-1)%self.__length],
-                                self.__grid[(i-1)%self.__height][(j-1)%self.__length],
-                            ]
-                node.link( linkArray )
+        #Defining lambda here for more readability
+        linkLambda = lambda i, j : self.__grid[i][j].link(
+                    #Defining the array of link
+                    [
+                        self.__grid[(i-1)%self.__height][(j  )%self.__length], #North
+                        self.__grid[(i-1)%self.__height][(j+1)%self.__length], #NorthEast
+                        self.__grid[(i  )%self.__height][(j+1)%self.__length], #East
+                        self.__grid[(i+1)%self.__height][(j+1)%self.__length], #SouthEast
+                        self.__grid[(i+1)%self.__height][(j  )%self.__length], #South
+                        self.__grid[(i+1)%self.__height][(j-1)%self.__length], #SouthWest
+                        self.__grid[(i  )%self.__height][(j-1)%self.__length], #West
+                        self.__grid[(i-1)%self.__height][(j-1)%self.__length], #NorthWest
+                    ] )
+        self.__forEachNode( linkLambda )
+        #print("Acceccing node[{}][{}]".format(i-1%self.__height, j  %self.__length))
+        #print("Acceccing node[{}][{}]".format(i-1%self.__height, j+1%self.__length))
+        #print("Acceccing node[{}][{}]".format(i  %self.__height, j+1%self.__length))
+        #print("Acceccing node[{}][{}]".format(i+1%self.__height, j+1%self.__length))
+        #print("Acceccing node[{}][{}]".format(i+1%self.__height, j  %self.__length))
+        #print("Acceccing node[{}][{}]".format(i+1%self.__height, j-1%self.__length))
+        #print("Acceccing node[{}][{}]".format(i  %self.__height, j-1%self.__length))
+        #print("Acceccing node[{}][{}]".format(i-1%self.__height, j-1%self.__length))
 
-    #Todo: make a lambda that allows the use of forEachNode
+    #Todo: make a lambda that allows the use of __forEachNode
     def printGrid( self ):
+        #testLambda = lambda i, j : self.__grid[i][j].print()
         for i in range( self.__height ):
             row = []
             for j in range( self.__height ):
